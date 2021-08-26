51f5598d6cf42eeb172d8ee028bf68a876218992
Aleem Haq <aleemhaq@Aleems-MBP.cogeco.local>
2021-08-26 10:10:10 -0400

    Adding older test to views

--- tests/tests_snippets/test_views.py
+++ tests/tests_snippets/test_views.py
@@ -1,11 +1,27 @@
 import pytest
 
+import json
+
+from django.urls import reverse
+
+from snippets.views import SnippetViewSet
+
 class TestSnippetApiViews():
 
     #Test:
     #The response status code
     #The response content
+    # snippets created/remove
     
     def test_get_snippet_list(self):
         assert True
+
+    def test_result_finished(self, rf):
+        request = rf.get(reverse('snippetviewset'))
+        response = SnippetViewSet.as_view()(request)
+
+        assert response.status_code == 200
+
+        content = json.loads(response.content)
+        assert content['result'] == 'FINISHED'
      
