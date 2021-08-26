c1f32a46c24679520834cd8be5485fd4b3d070d8
Aleem Haq <aleemhaq@Aleems-MBP.cogeco.local>
2021-08-26 09:59:25 -0400

    basic tests to add to testSnippetViews

--- tests/tests_snippets/test_views.py
+++ tests/tests_snippets/test_views.py
@@ -1,7 +1,11 @@
 import pytest
 
 class TestSnippetApiViews():
+
+    #Test:
+    #The response status code
+    #The response content
     
-    def test_first(self):
+    def test_get_snippet_list(self):
         assert True
      
